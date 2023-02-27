fd0b34b84af8b5af5440f10e93edbdce81387dbe
humrook <44312216+humrook@users.noreply.github.com>
2023-02-27 16:39:28 -0500

    Final Commit

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-/*
+
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 let count=0
 
 function increment()
 {
 	count+=1
-	countEl.innerText=count
-
+	//countEl.innerText=count
+	countEl.textContent=count
 }
 
 // Create a function , save()which logs the count when it's called
 
 function save()
 {
-	console.log(count)
-	console.log(countEl)
+//Create a variable that contains both the count and hyphen;Render the variable in the saveEl using innerText
+let stringcount = count+' - '
+	//saveEl.innerText += stringcount
+	saveEl.textContent += stringcount
+	count=0								//after save, reset count to 0 
+	countEl.textContent=count		//display back in html the count()
 }
-*/
 
-let name="Nathan Jerome Jockey"
-let greeting="Hi, my name is"
-
-let myGreeting = greeting+" "+name
-console.log(myGreeting)
+console.log("Let's count people on a bus stop")
